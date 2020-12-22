ebfeb675bf097346c7b0af4ed1a8c79d47f3ce00
rpźbass <ronaldop.tech@gmail.com>
2020-12-22 11:54:53 -0300

    stringbuilder

---
+++ src/Application/Program.java
@@ -0,0 +1,31 @@
+package Application;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
+import entities.Comment;
+import entities.Post;
+
+public class Program {
+
+	public static void main(String[] args) throws ParseException {
+		
+		
+		Scanner input = new Scanner(System.in);
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		
+		Comment c1 = new Comment("Have nice a trip!!!"); //No estilo hardcode sem fontes externas
+		Comment c2 = new Comment("Wow that's awesome");  
+		
+		
+		Post p1 = new Post(sdf.parse("25/12/2012 11:30:45"),"Travel a new Zeland", "HAVE NICE TRIP!!!",12);
+		
+		p1.addComment(c1);
+		p1.addComment(c2);
+		
+		System.out.println(p1);
+
+	}
+
+}
